d90e72b2d04f91de26ba428a216291472febf432
Metato <160302877+Kanghs0212@users.noreply.github.com>
2024-07-28 14:29:37 +0900

    [Gold IV] Title: 무기 공학, Time: 284 ms, Memory: 20752 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.java
+++ 백준/Gold/18430. 무기 공학/무기 공학.java
@@ -11,14 +11,18 @@ public class Main {
     static boolean[][] check;
     static int[][] d1 = {{-1,0,1,0},{0,1,0,-1}};
     static int[][] d2 = {{0,1,0,-1},{1,0,-1,0}};
-    public static void find(int index,int sum){
-        if(index==N*M){
-            best = Math.max(best,sum);
-            return;
+    public static void find(int x,int y,int sum){
+        if(y==M){
+            if(x==N-1){
+                best=Math.max(best,sum);
+                return;
+            }
+            else{
+                x++;
+                y=0;
+            }
         }
 
-        int x = index/M;
-        int y = index%M;
 
         for (int i = 0; i < 4; i++) {
 
@@ -30,19 +34,18 @@ public class Main {
                || check[x1][y1] || check[x2][y2] || check[x][y]){
                 continue;
             }
-
             check[x][y]=true;
             check[x1][y1]=true;
             check[x2][y2]=true;
 
-            find(index+1,sum + map[x][y]*2 + map[x1][y1] + map[x2][y2]);
+            find(x,y+1,sum + map[x][y]*2 + map[x1][y1] + map[x2][y2]);
 
             check[x][y]=false;
             check[x1][y1]=false;
             check[x2][y2]=false;
         }
 
-        find(index+1,sum);
+        find(x, y+1, sum);
 
 
     }
@@ -64,7 +67,7 @@ public class Main {
             }
         }
 
-        find(0,0);
+        find(0,0,0);
         System.out.println(best);
 
     }
