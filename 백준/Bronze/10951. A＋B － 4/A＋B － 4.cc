1a9f54b2a7456f7775a8809d83ea492841399316
Metato <160302877+Kanghs0212@users.noreply.github.com>
2025-11-07 16:55:34 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <iostream>
+#include<string>
+using namespace std;
+
+
+int main() {
+	int e1=0;
+	int e2=0;
+	
+	while (cin >> e1 >> e2)
+	{
+		cout << e1 + e2 << endl;
+	}
+	
+	return 0;
+}
